a15bb4c4fc6e7d7f5cf02d2958d9dd9b46e5fec6
mursystem <mursystem@gmail.com>
2023-06-17 01:38:30 +0200

    tester commit

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
+// Import the WebSocket library
 const WebSocket = require('ws');
-const uuid = require('uuid');
 
 // Create a WebSocket server
 const wss = new WebSocket.Server({ port: 3000 });
 
 // Store connected clients
-const clients = new Set();
+const clients = {};
 
-// Broadcast a message to all connected clients
-function broadcast(message) {
-  clients.forEach((client) => {
-    client.send(message);
-  });
-}
-
-// Event listener for new connections
+// Handle incoming connections
 wss.on('connection', (ws) => {
-  console.log('A new client has connected.');
+  // Assign a unique identifier to the client
+  const id = Math.random().toString(36).substring(2);
+  clients[id] = ws;
+  ws.send(JSON.stringify({ type: 'id', data: id }));
 
-  // Generate a random ID for the user
-  const userId = uuid.v4();
-
-  // Add the new client to the set of connected clients
-  clients.add(ws);
-
-  // Send a welcome message to the new user
-  ws.send(`Welcome! Your ID is: ${userId}`);
-
-  // Notify all clients that a new user has joined
-  broadcast(`User ${userId} has joined.`);
-
-  // Event listener for incoming messages
+  // Handle incoming messages
   ws.on('message', (message) => {
-    console.log(`Received message from ${userId}:`, message);
-
-    // Example: broadcast the received message to all clients
-    broadcast(`User ${userId} says: ${message}`);
+    const data = JSON.parse(message);
+    switch (data.type) {
+      case 'offer':
+        // Broadcast the offer to all other clients
+        Object.keys(clients).forEach((clientId) => {
+          if (clientId !== id) {
+            clients[clientId].send(message);
+          }
+        });
+        break;
+      case 'answer':
+        // Send the answer back to the original client
+        clients[data.to].send(message);
+        break;
+      case 'candidate':
+        // Broadcast the ICE candidate to all other clients
+        Object.keys(clients).forEach((clientId) => {
+          if (clientId !== id) {
+            clients[clientId].send(message);
+          }
+        });
+        break;
+      default:
+        break;
+    }
   });
 
-  // Event listener for connection close
+  // Handle disconnections
   ws.on('close', () => {
-    console.log(`Client ${userId} disconnected.`);
-    
-    clients.delete(ws);
-    broadcast(`User ${userId} has left.`);
+    delete clients[id];
   });
 });
-
-console.log('WebSocket server is running on port 3000.');
