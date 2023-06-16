c1c9e7471acdbf988b1e37f8246e2283776b4ddd
mursystem <mursystem@gmail.com>
2023-06-17 00:18:50 +0200

    tester commit

--- index.js
+++ index.js
@@ -1,45 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const expressWs = require('express-ws');
+const WebSocket = require('ws');
 
-const app = express();
-const port = process.env.PORT || 3000; // Use the environment variable 'PORT' if available, otherwise default to 3000
+// Create a WebSocket server
+const wss = new WebSocket.Server({ port: 3000 });
 
-// Enable WebSocket support
-const wsInstance = expressWs(app);
+// Event listener for new connections
+wss.on('connection', (ws) => {
+  console.log('A new client has connected.');
 
-app.use(cors());
-
-// Serve the static files from the 'public' directory
-app.use(express.static('public'));
-
-// Serve the 'index.html' file from the root URL
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// WebSocket endpoint
-app.ws('/websocket', (ws, req) => {
-  // WebSocket connection established
-  console.log('WebSocket connection established');
-
-  // Handle incoming messages
+  // Event listener for incoming messages
   ws.on('message', (message) => {
     console.log('Received message:', message);
-    // Process and handle the received message
-
-    // Example: Echo the message back to the client
-    ws.send(message);
   });
 
-  // Handle WebSocket connection closure
+  // Event listener for connection close
   ws.on('close', () => {
-    console.log('WebSocket connection closed');
-    // Handle connection closure
+    console.log('Client disconnected.');
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+console.log('WebSocket server is running on port 3000.');
