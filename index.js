66791cb710f15cca771020395942c081fa641a86
mursystem <mursystem@gmail.com>
2023-06-17 23:15:56 +0200

    tester commit

--- index.js
+++ index.js
@@ -51,6 +51,25 @@ wss.on('connection', (ws) => {
   // Event listener for incoming messages
   ws.on('message', (message) => {
     console.log(`Received message from ${userId}:`, message);
+    // Check if the message is an offer from Peer A
+    const parsedMessage = JSON.parse(message);
+    if (parsedMessage.type === 'offer' && parsedMessage.sender === 'peerA') {
+      // Find the WebSocket connection of Peer B
+      const peerBConnection = Array.from(clients).find((client) => {
+        // Skip the connection of Peer A
+        if (client !== ws && clients.has(client)) {
+          const clientUserId = client._socket.remoteAddress + ':' + client._socket.remotePort;
+          return clientUserId === peerB;
+        }
+        return false;
+      });
+
+      // Send the offer to Peer B
+      if (peerBConnection) {
+        peerBConnection.send(JSON.stringify(parsedMessage));
+      }
+    }
+
   });
 
   // Event listener for connection close
