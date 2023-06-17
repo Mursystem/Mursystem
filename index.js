e792d83a62decf0f6ac191277855a7289b8f1ff3
mursystem <mursystem@gmail.com>
2023-06-17 23:34:38 +0200

    tester commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received message from ${userId}:`, message);
     // Check if the message is an offer from Peer A
-    const parsedMessage = JSON.parse(message);
+    const parsedMessage = (message);
     if (parsedMessage.type === 'offer' && parsedMessage.sender === 'peerA') {
       // Find the WebSocket connection of Peer B
       const peerBConnection = Array.from(clients).find((client) => {
@@ -66,7 +66,7 @@ wss.on('connection', (ws) => {
 
       // Send the offer to Peer B
       if (peerBConnection) {
-        peerBConnection.send((parsedMessage));
+        peerBConnection.send(JSON.stringify(parsedMessage));
       }
     }
 
