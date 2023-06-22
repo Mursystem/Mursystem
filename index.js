852e01546d72a2017e30196c1ba53415d7f50781
mursystem <mursystem@gmail.com>
2023-06-22 18:54:30 +0200

    tester commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const WebSocket = require('ws');
-const uuid = require('uuid');
 // Create a WebSocket server
 const wss = new WebSocket.Server({ port: 3000 });
 // Store connected clients
@@ -16,11 +15,6 @@ function broadcast(message) {
 // Event listener for new connections
 wss.on('connection', (ws) => {
   console.log('A new client has connected.');
-
-  // Generate a random ID for the user
-  const userId = uuid.v4();
-  // Add the new client to the set of connected clients
-  clients.add(ws);
   // Check if both peers are already connected
   if (peerA && peerB) {
     ws.send(JSON.stringify({ type: 'reject', message: 'Maximum connections reached.' }));
@@ -29,42 +23,46 @@ wss.on('connection', (ws) => {
   }
   // Set the role for the current peer
   if (!peerA) {
-    peerA = userId;
+    peerA = ws;
     ws.send(JSON.stringify({ type: 'role', role: 'peerA' }));
+    ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!'  }));
   } else if (!peerB) {
-    peerB = userId;
+    peerB = ws;
     ws.send(JSON.stringify({ type: 'role', role: 'peerB' }));
+    ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!'  }));
   }
   // Send a welcome message to the new user
-  ws.send(JSON.stringify({ type: 'welcome', message: `Welcome! Your ID is: ${userId}` }));
-  // Notify all clients that a new user has joined
-  broadcast({ type: 'join', userId });
+  ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!'  }));
+
   // Event listener for incoming messages
-  ws.on('message', (message) => {
+  peerA.on('message', (message) => {
     const parsedMessage = JSON.parse(message);
-    console.log(`Received message from ${userId}:`, parsedMessage);
     if (parsedMessage.type === 'offer') {
       savedOffer = parsedMessage.offer;
     }
-    if (parsedMessage.type === 'ready') {
-      broadcast({ type: 'offer', offer: savedOffer });
-    }
-
     if (parsedMessage.type === 'answer') {
-      broadcast({ type: 'answer', answer: parsedMessage.answer });
+      peerB.send({ type: 'answer', answer: parsedMessage.answer });
     }
   });
+
+  peerB.on('message', (message) => {
+    const parsedMessage = JSON.parse(message);
+    
+    if (parsedMessage.type === 'ready') {
+      peerB.send({ type: 'offer', offer: savedOffer });
+    }
+  });
+  
   // Event listener for connection close
-  ws.on('close', () => {
-    console.log(`Client ${userId} disconnected.`);
-    clients.delete(ws);
-    broadcast({ type: 'leave', userId });
-    // Reset the role if a peer disconnects
-    if (userId === peerA) {
-      peerA = null;
-    } else if (userId === peerB) {
-      peerB = null;
-    }
-  });
+  peerA.on('close', () => {
+    peerB.send({ type: 'leave', message: "peer A left" });
+    peerA = null;
+  })
+  peerB.on('close', () => {
+    peerA.send({ type: 'leave', message: "peer B left" });
+    peerA = null;
+  })
+
+
 });
 console.log('WebSocket server is running on port 3000.');
