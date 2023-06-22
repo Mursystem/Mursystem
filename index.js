0738a9443a760860704e2dc961e326a7b6785c87
mursystem <mursystem@gmail.com>
2023-06-22 19:17:01 +0200

    tester commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 // Create a WebSocket server
 const wss = new WebSocket.Server({ port: 3000 });
+console.log('WebSocket server is running on port 3000.');
 // Store connected clients
 const clients = new Set();
 let peerA = null;
@@ -24,45 +25,38 @@ wss.on('connection', (ws) => {
   // Set the role for the current peer
   if (!peerA) {
     peerA = ws;
+    peerA.on('message', (message) => {
+      const parsedMessage = JSON.parse(message);
+      if (parsedMessage.type === 'offer') {
+        savedOffer = parsedMessage.offer;
+      }
+      if (parsedMessage.type === 'answer') {
+        peerB.send({ type: 'answer', answer: parsedMessage.answer });
+      }
+    });
+    peerA.on('close', () => {
+      peerB.send({ type: 'leave', message: "peer A left" });
+      peerA = null;
+    })
     ws.send(JSON.stringify({ type: 'role', role: 'peerA' }));
-    ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!'  }));
+    ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!' }));
   } else if (!peerB) {
     peerB = ws;
+    peerB.on('message', (message) => {
+      const parsedMessage = JSON.parse(message);
+      if (parsedMessage.type === 'ready') {
+        peerB.send({ type: 'offer', offer: savedOffer });
+      }
+    });
+    peerB.on('close', () => {
+      peerA.send({ type: 'leave', message: "peer B left" });
+      peerA = null;
+    })
     ws.send(JSON.stringify({ type: 'role', role: 'peerB' }));
-    ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!'  }));
+    ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!' }));
   }
   // Send a welcome message to the new user
-  ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!'  }));
-
-  // Event listener for incoming messages
-  peerA.on('message', (message) => {
-    const parsedMessage = JSON.parse(message);
-    if (parsedMessage.type === 'offer') {
-      savedOffer = parsedMessage.offer;
-    }
-    if (parsedMessage.type === 'answer') {
-      peerB.send({ type: 'answer', answer: parsedMessage.answer });
-    }
-  });
-
-  peerB.on('message', (message) => {
-    const parsedMessage = JSON.parse(message);
-    
-    if (parsedMessage.type === 'ready') {
-      peerB.send({ type: 'offer', offer: savedOffer });
-    }
-  });
-  
-  // Event listener for connection close
-  peerA.on('close', () => {
-    peerB.send({ type: 'leave', message: "peer A left" });
-    peerA = null;
-  })
-  peerB.on('close', () => {
-    peerA.send({ type: 'leave', message: "peer B left" });
-    peerA = null;
-  })
-
-
+  ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome!' }));
 });
-console.log('WebSocket server is running on port 3000.');
+
+
